3cfd1408d6bc42af1b6bbbd37ee1a3554d125328
Brad Langshaw <200243850@student.georgianc.on.ca>
2016-12-16 15:45:53 -0500

    yup

---
+++ startapp.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
+<?php
+$username = intval($_GET['username']);
+
+$sql="INSERT INTO [dbo].[usernames]('user') VALUES ("".$username."")";
+$sql=."SELECT * FROM [dbo].[usernames] WHERE user = "".$username.""";
+
+$result = mysqli_query($conn,$sql);
+
+try {
+    $conn = new PDO("sqlsrv:server = tcp:project2-chatroom.database.windows.net,1433; Database = ChatRoom", "BradGit", "MemberThe90s");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+$connectionInfo = array("UID" => "BradGit@project2-chatroom", "pwd" => "MemberThe90s", "Database" => "ChatRoom", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:project2-chatroom.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+while($row = mysqli_fetch_array($result)) {
+}
+echo "</table>";
+mysqli_close($con);
+?>
+</body>
+</html>
